102c84024387e3e6383976655428035b2e1f7b01
Bilal59170 <bilal.ben-moussa@eleves.enpc.fr>
2023-11-16 18:31:51 +0100

    feat:bouton_Next_Track

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">2eme ytrfc</h1>
-      </header>
-      <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
-      </div>
-      <div className="App-buttons"></div>
-    </div>
-  );
-};
 
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
@@ -24,6 +11,36 @@ const trackUrls = [
   'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
 ];
 
-<audio src={trackUrls[0]} autoPlay controls />
+
+const App = () => {
+
+
+  const [trackIndex, setTrackIndex] = useState(0)
+  const goToNextTrack = () => {
+    setTrackIndex(trackIndex + 1);
+  }
+
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">2eme ytrfc</h1>
+      </header>
+      <div className="App-images">
+        <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
+        <audio src={trackUrls[trackIndex]} autoPlay controls />
+        <button onClick={goToNextTrack}>
+          Next track
+        </button>
+      </div>
+      <div className="App-buttons"></div>
+    </div>
+    
+  );
+};
+ 
 
 export default App;
